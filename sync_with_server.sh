fd7e55ab4ce512e8d3cae354017175c595b4f7e9
Dixin <you@example.com>
2021-12-03 18:18:41 -0800

    small fix

--- sync_with_server.sh
+++ sync_with_server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-TPCH_JAR=/home/totemtang/slothdb/spark/middle-ground/target/scala-2.11/totem-middle-ground_2.11-2.4.0.jar
-SQL_JAR=/home/totemtang/slothdb/spark/sql/core/target/scala-2.11/spark-sql_2.11-2.4.0.jar
-EXAMPLE_JAR=/home/totemtang/slothdb/spark/examples/target/scala-2.11/jars/spark-examples_2.11-2.4.0.jar
-CAT_JAR=/home/totemtang/slothdb/spark/sql/catalyst/target/scala-2.11/spark-catalyst_2.11-2.4.0.jar
-AVRO_JAR=/home/totemtang/slothdb/spark/external/avro/target/scala-2.11/spark-avro_2.11-2.4.0.jar
-KAFAK_JAR=/home/totemtang/slothdb/spark/external/kafka-0-10-sql/target/scala-2.11/spark-sql-kafka-0-10_2.11-2.4.0.jar
+SPARK_HOME=/home/totemtang/slothdb/inqp
+
+TPCH_JAR=$SPARK_HOME/middle-ground/target/scala-2.11/totem-middle-ground_2.11-2.4.0.jar
+SQL_JAR=$SPARK_HOME/sql/core/target/scala-2.11/spark-sql_2.11-2.4.0.jar
+CAT_JAR=$SPARK_HOME/sql/catalyst/target/scala-2.11/spark-catalyst_2.11-2.4.0.jar
+AVRO_JAR=$SPARK_HOME/external/avro/target/scala-2.11/spark-avro_2.11-2.4.0.jar
+KAFAK_JAR=$SPARK_HOME/external/kafka-0-10-sql/target/scala-2.11/spark-sql-kafka-0-10_2.11-2.4.0.jar
 
 
 #REMOTE=lincoln.cs.uchicago.edu
@@ -15,12 +16,7 @@ REMOTE_SPARK=$REMOTE:/tank/hdfs/totem/slothdb/spark
 #REMOTE=grace.cs.uchicago.edu
 #REMOTE_SPARK=$REMOTE:/mnt/hdd-2T-1/totem/spark/
 
-#scp $TPCH_JAR $SQL_JAR $EXAMPLE_JAR $REMOTE_SPARK/jars/
-scp $TPCH_JAR $SQL_JAR $REMOTE_SPARK/jars/
-#scp $TPCH_JAR $REMOTE_SPARK/jars/
-#scp $CAT_JAR $REMOTE_SPARK/jars/
-#scp $TPCH_JAR $SQL_JAR $REMOTE_SPARK/jars/
-#scp $SQL_JAR $REMOTE_SPARK/jars/
-#scp $CAT_JAR $REMOTE_SPARK/jars/
+scp $TPCH_JAR $SQL_JAR $REMOTE_SPARK/jars
+scp $CAT_JAR $AVRO_JAR $KAFKA_JAR $REMOTE_SPARK/jars
 
 
